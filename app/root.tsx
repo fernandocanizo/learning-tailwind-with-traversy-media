0aec36ac2770b7f1889efbfdc8ab24dbb42ad02b
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-05-03 20:34:59 -0300

    Add ErrorBoundary to root

--- app/root.tsx
+++ app/root.tsx
@@ -2,17 +2,19 @@ import style from "~/style.css?url"
 
 import type { LinksFunction } from "@remix-run/node"
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: style },
-]
-
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+  useRouteError,
+  isRouteErrorResponse,
+} from "@remix-run/react"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: style },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -29,9 +31,40 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError()
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <>
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </>
+    )
+  }
+
+  if (error instanceof Error) {
+    return (
+      <>
+        <h1>Error!</h1>
+        <p>{error?.message ?? "Unknown error"}</p>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h1>Unknown error type</h1>
+      <p>{JSON.stringify(error)}</p>
+    </>
+  )
 }
