ce14c951fe0e64beeb264a4d14261f2c04a16abc
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-05-04 12:58:27 -0300

    Add header menus

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,7 +12,17 @@ export default function Index() {
     <nav className="relative container mx-auto p-6 bg-slate-300 rounded-md drop-shadow-md">
       <div className="flex items-center justify-between">
         <div className="pt-2">
-          <img src="img/logo.svg" alt="Logo Manage" />
+          <a href="/">
+            <img src="img/logo.svg" alt="Logo Manage" />
+          </a>
+        </div>
+
+        <div className="hidden md:flex space-x-6">
+          <a href="#pricing" className="hover:text-darkGrayishBlue">Pricing</a>
+          <a href="#product" className="hover:text-darkGrayishBlue">Product</a>
+          <a href="#about" className="hover:text-darkGrayishBlue">About Us</a>
+          <a href="#careers" className="hover:text-darkGrayishBlue">Careers</a>
+          <a href="#community" className="hover:text-darkGrayishBlue active:bg-orange-300">Community</a>
         </div>
       </div>
     </nav>
